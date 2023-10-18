f95345d10570a8717cbdb8d5528b1031f0604281
João Paulo <jp_cbc@hotmail.com>
2023-10-18 00:55:08 -0300

    Variáveis para conexão

--- src/main/java/imobiliaria/util/Conexao.java
+++ src/main/java/imobiliaria/util/Conexao.java
@@ -8,9 +8,9 @@ public class Conexao {
 
 	public Connection conectar() throws SQLException{
 		try {
-			Class.forName("com.mysql.cj.jdbc.Driver");
+			Class.forName("com.mysql.jdbc.Driver");
 			
-			return DriverManager.getConnection("jdbc:mysql://localhost:3306");
+			return DriverManager.getConnection("jdbc:mysql://localhost:3306/imobiliaria?useTimezone=true&serverTimezone=UTC&user=root&password=");
 		} catch (ClassNotFoundException e) {
 			throw new RuntimeException(e);
 		}
--- webapp/config.jsp
+++ webapp/config.jsp
@@ -4,4 +4,10 @@
 	String whatsapp2 = "(91) 98114-0021";
 	String fixo = "(91) 99999-9999";
 	String whatsappLink = "5591981464025";
+	
+// 	Configuração de banco de dados
+	String servidor = "localhost";
+	String banco = "imobiliaria";
+	String usuario = "root";
+	String senha = "";
 %>
